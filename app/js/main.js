3c352b9a8186a4f988085b8d0e3fd53e7402f417
Vitalii Fetisov <neverest1827@gmail.com>
2022-07-28 19:41:04 +0300

    added product list1 and product list2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,25 @@
 $(function () {
+    $(".star").rateYo({
+        starWidth: "11px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true
+    });
+
+    $(".price-filters__input").ionRangeSlider({
+        prefix: "$",
+        prettify_enabled: true,
+        prettify_separator: ".",
+        onStart: function (data) {
+            $(".price-filters__from").text(data.from);
+            $(".price-filters__to").text(data.to);
+        },
+        onChange: function (data) {
+            $(".price-filters__from").text(data.from);
+            $(".price-filters__to").text(data.to);
+        },
+    });
+
     $(".header-slider__top").slick({
         dots: true,
         arrows: false,
